83a41e62ea5a2f222a331ea7429bf9861986dbdb
Martin Burch <MartinB@mmc.school.nz>
2020-06-15 15:29:49 +1200

    SetUp Form

--- FrmDodge.cs
+++ FrmDodge.cs
@@ -3,19 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace _2020Level2_Dodge
 {
-    public partial class Form1 : Form
+    public partial class FrmDodge : Form
     {
-        public Form1()
+        public FrmDodge()
         {
             InitializeComponent();
         }
-
     }
 }
